bdbb6ced375b50f7b64f8524b1d4c3a8714090d4
Eddy Bautista <bautista.j.eddy@gmail.com>
2019-11-28 08:05:03 -0600

    formatted code

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
 const path = require('path');
 
-//create folder
+// create folder
 
-// fs.mkdir(path.join(__dirname, '/test'), {}, err => {
-//     if(err) throw err;
-//     console.log('Folder created...');
-// });
+fs.mkdir(path.join(__dirname, '/test'), {}, err => {
+    if(err) throw err;
+    console.log('Folder created...');
+});
 
 // Create and write to file
 
@@ -15,4 +15,18 @@ fs.writeFile(path.join(__dirname, '/test', 'hello.txt'), "Hello, Node", err => {
     console.log('File written too...');
 }
 );
+
+// Read file
+
+fs.readFile(path.join(__dirname, '/test', 'hello.txt'), 'utf8', (err, data) => {
+    if(err) throw err;
+    console.log(data);
+});
+
+// Rename file
+
+fs.rename(path.join(__dirname, '/test', 'hello.txt'), path.join(__dirname, '/test', 'helloNode.txt'), (err) => {
+        if(err) throw err;
+        console.log('File rename');
+    });
 
