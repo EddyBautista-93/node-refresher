ebdbad595e277def2e6ded1cf79b6307d2373360
Eddy Bautista <bautista.j.eddy@gmail.com>
2019-11-28 07:46:08 -0600

    more references

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -3,8 +3,16 @@ const path = require('path');
 
 //create folder
 
-fs.mkdir(path.join(__dirname, '/test'), {}, err => {
+// fs.mkdir(path.join(__dirname, '/test'), {}, err => {
+//     if(err) throw err;
+//     console.log('Folder created...');
+// });
+
+// Create and write to file
+
+fs.writeFile(path.join(__dirname, '/test', 'hello.txt'), "Hello, Node", err => {
     if(err) throw err;
-    console.log('Folder created...');
-});
+    console.log('File written too...');
+}
+);
 
